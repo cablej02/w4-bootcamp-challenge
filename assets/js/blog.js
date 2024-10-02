650665f0b4f18384926c089017efa37bdb0c14f1
Jason Cable <cablej02@gmail.com>
2024-10-02 15:17:31 -0500

    bug fixes for blog post additions to page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,18 +4,22 @@ const backEl = document.querySelector('#back');
 
 // Create a function that builds an element and appends it to the DOM
 const addBlogPost = (blogPostObj) =>{
+    // Create the elements
     const articleEl = document.createElement('article');
     const h2El = document.createElement('h2');
     const blockquoteEl = document.createElement('blockquote');
     const pEl = document.createElement('p');
 
+    // Add the content to the elements
     h2El.textContent = blogPostObj.title;
     blockquoteEl.textContent = blogPostObj.content;
     pEl.textContent = `Posted by: ${blogPostObj.username}`;
-
-    articleEl.append(h2El, blockquoteEl, pEl);
     articleEl.classList.add('card');
 
+    // Append the elements to the article element
+    articleEl.append(h2El, blockquoteEl, pEl);
+
+    // Append the article element to the main element
     mainEl.appendChild(articleEl);
 }
 
@@ -28,13 +32,19 @@ const handleNoPosts = () => {
 
 // Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
 const renderBlogList = () => {
+    // Clear the main element
+    mainEl.innerHTML = '';
+
+    // Get the blog data from local storage
     const blogArr = readLocalStorage();
 
+    // Check if the blog data is empty
     if (blogArr.length === 0){
         handleNoPosts();
         return;
     }
 
+    // Loop through the blog data and add each post to the DOM
     blogArr.forEach((blogPostObj) => addBlogPost(blogPostObj));
 }
 
